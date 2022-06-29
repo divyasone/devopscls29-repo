1ed9818ec74b5e35c7f885956cf9dde6d862ef57
divyasone <divyakshathriyas@gmail.com>
2022-06-29 16:22:43 +0530

    update demo bash file

--- demo.sh
+++ demo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "*************Display root F.s.space**************"
+echo "***********Display root F.s.space****************"
 df -h
 echo "*************Display todat date******************"
 date
-
+echo "**********Display jenkins memomry space**********"
+free -m
